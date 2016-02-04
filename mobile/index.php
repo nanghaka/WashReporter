6d60c69fb5e636f79d44bd582b69ba2dca729fb1
Daniel Nanghaka <dndannang@gmail.com>
2016-02-04 18:47:59 +0300

    Created Wash Reporter Mobile Repo
    This is the mobile branch of the main site. This includes a mobile api

---
+++ mobile/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wash Reporter Mobile</title>
+</head>
+<body>
+
+<h1>WashReporter Mobile</h1>
+
+This is the mobile platform for the Wash Reporter App
+
+</body>
+</html>
