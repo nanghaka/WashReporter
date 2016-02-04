34d774d44c636c640170163f24eb60ed109ce301
Daniel Nanghaka <dndannang@gmail.com>
2016-02-04 18:50:40 +0300

    Created the Main Page
    This is the landing page of Wash Reporter

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wash Reporter</title>
+</head>
+<body>
+
+<h1>WashReporter</h1>
+
+This is the platform to enhance global report on Water Sanitation and Health. 
+
+Anyone can be a WASH Reporter
+
+</body>
+</html>
