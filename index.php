8b474936850ef7c8edc07801fa9fdd28a2762a85
Daniel Nanghaka <dndannang@gmail.com>
2016-02-04 21:57:35 +0300

    Created button
    This is a report button

--- index.php
+++ index.php
@@ -4,12 +4,24 @@
 	<title>Wash Reporter</title>
 </head>
 <body>
+<div class="header">
 
 <h1>WashReporter</h1>
 
+</div>
+
+<div>
 This is the platform to enhance global report on Water Sanitation and Health. 
 
 Anyone can be a WASH Reporter
+
+</div>
+
+<br>
+<div>
+<button> Report </button>
+</div>
+
 
 </body>
 </html>
