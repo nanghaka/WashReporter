b946dcd04588f8b6d5ef0b762606746a117683e3
Daniel Nanghaka <dndannang@gmail.com>
2016-02-04 23:23:40 +0300

    Demo design
    This is a demonstration design of Wash Reporter

--- mobile/index.php
+++ mobile/index.php
@@ -1,4 +1,134 @@
 <!DOCTYPE html>
+<html class="html" lang="en-US">
+ <head>
+
+  <script type="text/javascript">
+   if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["jquery-1.8.3.min.js", "museutils.js", "jquery.watch.js", "webpro.js", "musewpdisclosure.js", "index.css"], "outOfDate":[]};
+</script>
+  
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <meta name="generator" content="2015.0.2.310"/>
+  <title>Home</title>
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="css/site_global.css?4052507572"/>
+  <link rel="stylesheet" type="text/css" href="css/index.css?3878648065" id="pagesheet"/>
+  <!-- Other scripts -->
+  <script type="text/javascript">
+   document.documentElement.className += ' js';
+</script>
+   </head>
+ <body>
+
+  <div class="clearfix" id="page"><!-- column -->
+   <div class="position_content" id="page_position_content">
+    <div class="browser_width colelem" id="u75-bw">
+     <div id="u75"><!-- group -->
+      <div class="clearfix" id="u75_align_to_page">
+       <div class="clip_frame grpelem" id="u77"><!-- image -->
+        <img class="block" id="u77_img" src="images/logo_old-crop-u77.jpg" alt="" width="92" height="85"/>
+       </div>
+       <img class="grpelem" id="u83-5" alt="WASHReporter" width="277" height="46" src="images/u83-5.png"/><!-- rasterized frame -->
+       <img class="grpelem" id="u85-4" alt="Report Wash Crisis " width="209" height="26" src="images/u85-4.png"/><!-- rasterized frame -->
+       <img class="grpelem" id="u130-4" alt="ABOUT US" width="120" height="26" src="images/u130-4.png"/><!-- rasterized frame -->
+      </div>
+     </div>
+    </div>
+    <div class="clearfix colelem" id="pu87"><!-- group -->
+     <div class="grpelem" id="u87"><!-- custom html -->
+      
+<iframe class="actAsDiv" width="547" height="399" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;q=Adobe%20Systems%20Inc%2C%20Park%20Avenue%2C%20San%20Jose%2C%20CA&amp;aq=0&amp;ie=UTF8&amp;t=m&amp;z=12&amp;iwloc=A&amp;output=embed"></iframe>
+
+</div>
+     <div class="clearfix grpelem" id="pu129-4"><!-- column -->
+      <img class="colelem" id="u129-4" alt="Latest Reports" width="209" height="26" src="images/u129-4.png"/><!-- rasterized frame -->
+      <div class="TabbedPanelsWidget clearfix colelem" id="tab-panelu89"><!-- vertical box -->
+       <div class="TabbedPanelsTabGroup clearfix colelem" id="u103"><!-- horizontal box -->
+        <div class="TabbedPanelsTab clearfix grpelem" id="u108"><!-- horizontal box -->
+         <div class=" NoWrap clearfix grpelem" id="u110-4"><!-- content -->
+          <p>Water</p>
+         </div>
+        </div>
+        <div class="TabbedPanelsTab clearfix grpelem" id="u112"><!-- horizontal box -->
+         <div class=" NoWrap clearfix grpelem" id="u113-4"><!-- content -->
+          <p>Sanitation</p>
+         </div>
+        </div>
+        <div class="TabbedPanelsTab clearfix grpelem" id="u104"><!-- horizontal box -->
+         <div class=" NoWrap clearfix grpelem" id="u105-4"><!-- content -->
+          <p>Health</p>
+         </div>
+        </div>
+       </div>
+       <div class="TabbedPanelsContentGroup clearfix colelem" id="u90"><!-- stack box -->
+        <div class="TabbedPanelsContent clearfix grpelem" id="u95"><!-- group -->
+         <div class="clip_frame grpelem" id="u96"><!-- image -->
+          <img class="block" id="u96_img" src="images/road.jpg" alt="" width="133" height="89"/>
+         </div>
+         <div class="clearfix grpelem" id="u98-6"><!-- content -->
+          <p id="u98-2">Lorem ipsum</p>
+          <p id="u98-4">Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu viverra iaculis. In hac habitasse platea dictumst.&nbsp; In vehicula diam et mauris imperdiet aliquet.</p>
+         </div>
+        </div>
+        <div class="TabbedPanelsContent invi clearfix grpelem" id="u99"><!-- group -->
+         <div class="clip_frame grpelem" id="u101"><!-- image -->
+          <img class="block" id="u101_img" src="images/cactus.jpg" alt="" width="133" height="89"/>
+         </div>
+         <div class="clearfix grpelem" id="u100-6"><!-- content -->
+          <p id="u100-2">Mauris sit amet</p>
+          <p id="u100-4">Tortor, eget ornare urna. Duis varius tellus eros. Donec odio arcu, rutrum ac rutrum eget, bibendum ac enim. Phasellus hendrerit iaculis purus. Aliquam sit amet molestie odio. Sed commodo dictum consequat aenean in est.</p>
+         </div>
+        </div>
+        <div class="TabbedPanelsContent invi clearfix grpelem" id="u91"><!-- group -->
+         <div class="clip_frame grpelem" id="u93"><!-- image -->
+          <img class="block" id="u93_img" src="images/trees.jpg" alt="" width="133" height="89"/>
+         </div>
+         <div class="clearfix grpelem" id="u92-6"><!-- content -->
+          <p id="u92-2">Ut eget felis</p>
+          <p id="u92-4">Sed velit congue viverra. Sed porta mattis luctus. Curabitur feugiat pharetra sem eu iaculis. Phasellus venenatis volutpat arcu id placerat. Aliquam fringilla ligula eu purus lacinia at volutpat nunc malesuada. Nunc a augue ac orci tempus commodo.</p>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </div>
+    <div class="browser_width" id="u76-bw">
+     <div class="pinned-colelem" id="u76"><!-- simple frame --></div>
+    </div>
+    <div class="verticalspacer"></div>
+   </div>
+  </div>
+  <!-- JS includes -->
+  <script type="text/javascript">
+   if (document.location.protocol != 'https:') document.write('\x3Cscript src="http://musecdn2.businesscatalyst.com/scripts/4.0/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
+</script>
+  <script type="text/javascript">
+   window.jQuery || document.write('\x3Cscript src="scripts/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
+</script>
+  <script src="scripts/museutils.js?275725342" type="text/javascript"></script>
+  <script src="scripts/jquery.watch.js?3999102769" type="text/javascript"></script>
+  <script src="scripts/webpro.js?3883484123" type="text/javascript"></script>
+  <script src="scripts/musewpdisclosure.js?404074387" type="text/javascript"></script>
+  <!-- Other scripts -->
+  <script type="text/javascript">
+   $(document).ready(function() { try {
+(function(){var a={},b=function(a){if(a.match(/^rgb/))return a=a.replace(/\s+/g,"").match(/([\d\,]+)/gi)[0].split(","),(parseInt(a[0])<<16)+(parseInt(a[1])<<8)+parseInt(a[2]);if(a.match(/^\#/))return parseInt(a.substr(1),16);return 0};(function(){$('link[type="text/css"]').each(function(){var b=($(this).attr("href")||"").match(/\/?css\/([\w\-]+\.css)\?(\d+)/);b&&b[1]&&b[2]&&(a[b[1]]=b[2])})})();(function(){$("body").append('<div class="version" style="display:none; width:1px; height:1px;"></div>');
+for(var c=$(".version"),d=0;d<Muse.assets.required.length;){var f=Muse.assets.required[d],g=f.match(/([\w\-\.]+)\.(\w+)$/),k=g&&g[1]?g[1]:null,g=g&&g[2]?g[2]:null;switch(g.toLowerCase()){case "css":k=k.replace(/\W/gi,"_").replace(/^([^a-z])/gi,"_$1");c.addClass(k);var g=b(c.css("color")),h=b(c.css("background-color"));g!=0||h!=0?(Muse.assets.required.splice(d,1),"undefined"!=typeof a[f]&&(g!=a[f]>>>24||h!=(a[f]&16777215))&&Muse.assets.outOfDate.push(f)):d++;c.removeClass(k);break;case "js":k.match(/^jquery-[\d\.]+/gi)&&
+typeof $!="undefined"?Muse.assets.required.splice(d,1):d++;break;default:throw Error("Unsupported file type: "+g);}}c.remove();if(Muse.assets.outOfDate.length||Muse.assets.required.length)c="Some files on the server may be missing or incorrect. Clear browser cache and try again. If the problem persists please contact website author.",(d=location&&location.search&&location.search.match&&location.search.match(/muse_debug/gi))&&Muse.assets.outOfDate.length&&(c+="\nOut of date: "+Muse.assets.outOfDate.join(",")),d&&Muse.assets.required.length&&(c+="\nMissing: "+Muse.assets.required.join(",")),alert(c)})()})();
+/* body */
+Muse.Utils.transformMarkupToFixBrowserProblemsPreInit();/* body */
+Muse.Utils.prepHyperlinks(true);/* body */
+Muse.Utils.resizeHeight()/* resize height */
+Muse.Utils.initWidget('#tab-panelu89', function(elem) { return new WebPro.Widget.TabbedPanels(elem, {event:'click',defaultIndex:0}); });/* #tab-panelu89 */
+Muse.Utils.fullPage('#page');/* 100% height page */
+Muse.Utils.showWidgetsWhenReady();/* body */
+Muse.Utils.transformMarkupToFixBrowserProblems();/* body */
+} catch(e) { if (e && 'function' == typeof e.notify) e.notify(); else Muse.Assert.fail('Error calling selector function:' + e); }});
+</script>
+   </body>
+</html>
+
+
+<!-- <!DOCTYPE html>
 <html>
 <head>
 	<title>Wash Reporter Mobile</title>
@@ -10,4 +140,4 @@
 This is the mobile platform for the Wash Reporter App
 
 </body>
-</html>
+</html> -->
